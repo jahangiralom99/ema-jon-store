05d38ab069677a66bbbaafe845c025998cda3957
jahangir <jahangiralomtuf99@gmail.com>
2023-04-30 16:22:55 +0600

    addede header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css'; 
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <img src={logo} alt="" />
+            <div className='ul-container'>
+            <a href="/shop">Shop</a>
+            <a href="/order">Order</a>
+            <a href="/inventory">Inventory</a>
+            <a href="/login">Login</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
