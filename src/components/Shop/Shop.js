99b1548b05e84f82694c6cb970c206b7f8747881
jahangir <jahangiralomtuf99@gmail.com>
2023-05-04 11:37:19 +0600

    retrieve product from store cart

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 import './Shop.css'; 
 import Cart from '../Cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 const Shop = () => {
-
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
 
@@ -14,11 +14,30 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, [])
 
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const savedCart = []
+        // stap 1
+        for (const id in storedCart) {
+            // stap number 2 
+            const addedProduct = products.find(product => product.id === id);
+            // stap number 3
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // stap number 4 
+                savedCart.push(addedProduct);
+            }
+            setCart(savedCart)
 
+        }
+    }, [products])
     
     const handleAddToCart = (product ) => {
         const newCart = [...cart, product];
         setCart(newCart);
+
+        addToDb(product.id)
     }
 
 
