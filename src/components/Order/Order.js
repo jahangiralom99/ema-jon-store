3a41ebbed6046670fe04fb7daf240b95abaafff9
jahangir <jahangiralomtuf99@gmail.com>
2023-05-08 20:26:26 +0600

    delete from cart

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,31 +1,35 @@
-import React from 'react';
-import Cart from '../Cart/Cart';
-import { useLoaderData } from 'react-router-dom';
-import ReviewItem from '../ReviewItem/ReviewItem';
+import React, { useState } from "react";
+import Cart from "../Cart/Cart";
+import { useLoaderData } from "react-router-dom";
+import ReviewItem from "../ReviewItem/ReviewItem";
+import { removeFromDb } from "../../utilities/fakedb";
 
 const Order = () => {
+  const product = useLoaderData();
+  const [cart, setCart] = useState(product);
 
-    const product = useLoaderData();
-    console.log(product);
+  const hendelRemovedFromCart = (id) => {
+      const reamaining = cart.filter(pd => pd.id !== id);
+      setCart(reamaining);
+      removeFromDb(id)
+  };
 
-    return (
-        <div className='shop-container bg-white'>
-        <div className='product-container h-[107px] mx-auto'>
-                {
-                    product.map(cart => <ReviewItem
-                        key={cart.id}
-                        cart ={cart}
-                    ></ReviewItem>)
-                }
-                {
-                    product.map(cart => console.log(cart))
-                }
-        </div>
-        <div className='cart-container'>
-           <Cart cart={product}></Cart>
-        </div>
+  return (
+    <div className="shop-container bg-white">
+      <div className="product-container h-[107px] mx-auto">
+        {cart.map((cart) => (
+          <ReviewItem
+            key={cart.id}
+            cart={cart}
+            hendelRemovedFromCart={hendelRemovedFromCart}
+          ></ReviewItem>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Order;
--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,22 +1,26 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faTrashCan} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
-const ReviewItem = ({ cart }) => {
-  const { img, name, price, shipping } = cart;
+const ReviewItem = ({ cart ,hendelRemovedFromCart}) => {
+  const { img, name, price, shipping, id } = cart;
   return (
     <div className="flex items-center mt-6 border-2 p-2 rounded-md">
       <img className="w-[91px] rounded-lg" src={img} alt="" />
-        <div className="ml-4 grow">
-          <h2 className="font-bold">{name}</h2>
-         <p>
+      <div className="ml-4 grow">
+        <h2 className="font-bold">{name}</h2>
+        <p>
           Price: <span className="text-[#FF9900]">${price}</span>
-             </p>
-         <p>
-         Shipping Charge: <span className="text-[#FF9900]">${shipping}</span>
-         </p>
+        </p>
+        <p>
+          Shipping Charge: <span className="text-[#FF9900]">${shipping}</span>
+        </p>
       </div>
-      <button className="h-[55px] w-[55px] rounded-[50%] bg-[#EB57574D]"><span className="text-[#EB5757] "><FontAwesomeIcon icon={faTrashCan} /></span></button>
+      <button onClick={()=> hendelRemovedFromCart(id)} className="h-[55px] w-[55px] rounded-[50%] bg-[#EB57574D]">
+        <span className="text-[#EB5757] ">
+          <FontAwesomeIcon icon={faTrashCan} />
+        </span>
+      </button>
     </div>
   );
 };
